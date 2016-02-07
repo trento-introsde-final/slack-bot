5aa73b2eb625a98050ea93981314cfae6168af14
damiano.fossa <d.fossa.91@gmail.com>
2016-02-07 17:46:08 +0100

    test

--- test.php
+++ test.php
@@ -1,17 +1,32 @@
 <?php 
 
-$client = new SoapClient("https://process-centric-services.herokuapp.com/processCentricServices?wsdl");
 
-//var_dump($client->__getFunctions()); 
+
+//var_dump($client->__getTypes());
 
 $params = array (
-    "slack_user_id" => "UF1244",
+    "slack_user_id" => "U43F341",
+);
+
+
+try {
+    $client = new SoapClient("https://process-centric-services.herokuapp.com/processCentricServices?wsdl");
+    $response = $client->checkGoalStatus($params);
+} catch (SoapFault $fault) {
+    trigger_error("SOAP Fault: (faultcode: {$fault->faultcode}, faultstring: {$fault->faultstring})", E_USER_ERROR);
+}
+
+
+var_dump($response);
+
+
+/*$params = array (
+    "slack_user_id" => "UK12344",
     "user_name" => "damiano.fossa",
 );
 
-//$response = $client->__soapCall('initializeUser', array($params));
 $response = $client->initializeUser($params);
 
-echo "response " . $response->id;
+var_dump($response);*/
 
 ?>
