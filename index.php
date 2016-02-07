5e129e1ca90f0cd9d16ec8310e98341ffd34f257
damiano.fossa <d.fossa.91@gmail.com>
2016-02-07 21:53:23 +0100

    checkgoals

--- index.php
+++ index.php
@@ -95,11 +95,19 @@ if($trigger_word == "register"){
 		}
 	}
 	
-	$options = array(
+	/*$options = array(
 	    'http' => array(
 	        'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
 	        'method'  => 'POST',
 	        'content' => "{\"text\": " .$response->goal->messages[0]->content. "\n\n" .$message. "\n\n" .$response->goal->messages[2]->content. "\"}",
+	    ),
+	);*/
+
+	$options = array(
+	    'http' => array(
+	        'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+	        'method'  => 'POST',
+	        'content' => "{\"text\": " .$message. "\"}",
 	    ),
 	);
 	
