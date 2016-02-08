e5c436f5d23815a059da499ba090781a0cc321c1
damiano.fossa <d.fossa.91@gmail.com>
2016-02-08 14:56:40 +0100

    handling fault

--- index.php
+++ index.php
@@ -196,6 +196,7 @@ try {
 		// Close the cURL resource, and free system resources
 		curl_close($ch);
 	} elseif($trigger_word != "register" || $trigger_word != "run" || $trigger_word != "goalstatus" || $trigger_word != "setgoal") {
+		
 		$message = "Wrong command! These are the commands available: register,\n run [distance] [time] [calories],\n goalstatus,\n setgoal [goal_type] [target_value] [period] ";
 
 		$data = "payload=" . json_encode(array(
