6040dc7b672fabc4b6977e6e02c9e2bbc8c1b683
damiano.fossa <d.fossa.91@gmail.com>
2016-02-08 10:40:51 +0100

    checkgoals

--- index.php
+++ index.php
@@ -114,7 +114,7 @@ if($trigger_word == "register"){
 	    'http' => array(
 	        'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
 	        'method'  => 'POST',
-	        'content' => "{\"text\": " . $last_message . "\"}",
+	        'content' => "{\"text\": " . htmlspecialchars($last_message) . "\"}",
 	    ),
 	);
 
