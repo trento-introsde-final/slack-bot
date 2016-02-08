e02ac7ba8851b6c55c2aa19c9e057f65fb1d9b36
damiano.fossa <d.fossa.91@gmail.com>
2016-02-08 15:07:07 +0100

    handling fault and wrong commands

--- index.php
+++ index.php
@@ -197,7 +197,7 @@ try {
 		curl_close($ch);
 	} elseif(($trigger_word != "register" || $trigger_word != "run" || $trigger_word != "goalstatus" || $trigger_word != "setgoal") && $user_name != "slackbot") {
 		
-		$message = "Wrong command! These are the commands available:\n\n register,\n run [distance] [time] [calories],\n goalstatus,\n setgoal [goal_type] [target_value] [period] ";
+		$message = "Wrong command! These are the commands available:\n\n register\n run [distance] [time] [calories]\n goalstatus\n setgoal [goal_type] [target_value] [period]";
 
 		$data = "payload=" . json_encode(array(
 	        "channel"       =>  "#tests",
